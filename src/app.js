6e93f79a0786887c4dc2ef178504f02f462fb7e4
tomjerzy <kastytom@gmail.com>
2020-04-17 10:26:39 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -13,8 +13,8 @@ require('./passport')
 server.listen(config.port, () => {
 	console.log(`TAKAMALI server running on port ${config.port}`)
 });
-var mongoDB = 'mongodb://127.0.0.1/takamali';
-//var mongoDB = 'mongodb+srv://tom:kicc@2019!!@cluster0-odnlj.mongodb.net/takamali?retryWrites=true&w=majority'
+//var mongoDB = 'mongodb://127.0.0.1/takamali';
+var mongoDB = 'mongodb+srv://tom:kicc@2019!!@cluster0-odnlj.mongodb.net/takamali?retryWrites=true&w=majority'
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
